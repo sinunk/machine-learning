34b7ff480b9d8d1d6405875c75835a76af060ca4
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-09-27 18:50:49 -0500

    Delete unnecessary comments.

--- Project 1 Regression/Regressor.java
+++ Project 1 Regression/Regressor.java
@@ -2,18 +2,7 @@ import java.lang.*;
 
 public class Regressor extends SupervisedLearner
 {
-/*
-Modify the constructor of your Regressor class to require a Function to be passed as a parameter. 
-Store a reference to this Function in a member variable named model. 
-(As always, you are welcome to do things your own way, as long as your way works.) 
 
-Implement the predict method to concatenate the parameters, params, with the variables, in, 
-and call the evaluate method of model. (Note that model is not the objective function.) 
-
-Implement the train method to regress the parameters of the model to fit with features and labels. 
-To do this, instantiate one of your optimizers and call optimize. 
-Here is some Java-style example pseudocode:
-*/
 	
 	String name()
 	{
@@ -52,8 +41,5 @@ Here is some Java-style example pseudocode:
 		HillClimber hc = new HillClimber(features.cols()+1, features.cols()+1, ob);
 		hc.optimize(200, 50, 0.01);
 	}
-/*
-(For example, if your model is "y=mx+b", then "x" is a variable or feature, and "m" and "b" are parameters. 
-The HillClimber will visit many different points in the parameter-space to find the one that fits well with the data.)
-*/
+
 }
