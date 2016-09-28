f479a1b28a693bf0f3ebc9fdcc90fe1e0738195d
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-09-27 19:05:48 -0500

    Remove unnecessary comments.

--- Project 1 Regression/HillClimber.java
+++ Project 1 Regression/HillClimber.java
@@ -1,15 +1,5 @@
 public class HillClimber extends Optimizer 
 {
-/*
-This class should have two member variables, named something like cur and step. 
-Both of these should be vectors of doubles. 
-The constructor should let the user specify the size of these vectors, 
-and should initilize each element of cur to 0.0, and each element of step to 0.1. 
-Implement the iterate method according to the following pseudocode:
-(Note that the values 1.25 and 0.8 are not necessarily ideal. 
-You can make these parameters, if you like.)
-*/
-
 	double[] cur;
 	double[] step;
 	double smallest_error;
@@ -35,10 +25,6 @@ You can make these parameters, if you like.)
 		cur = new double[curSize];
 		for(int i = 0; i < curSize; i++)
 			cur[i] = 0.0;
-
-		//debugging
-		//cur[0] = 0.5;
-		//cur[1] = 2.0;
 		
 		step = new double[stepSize];			
 		for(int i = 0; i < stepSize; i++)
@@ -50,10 +36,7 @@ You can make these parameters, if you like.)
 	{
 		if (cur==null)
 		throw new RuntimeException("cur should not be null");
-		//System.out.print("Before: ");
-        //Vec.println(cur);
-		
-		
+
 		for(int i = 0; i < cur.length; i++)
 		{
 			error[0] = obj.evaluate(cur);
@@ -98,8 +81,6 @@ You can make these parameters, if you like.)
 			}
 		}
 		
-		//System.out.print("After: ");
-        //Vec.println(cur);
 		return smallest_error;
 	}
 
