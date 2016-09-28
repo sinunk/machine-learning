607eb74dac251b687e4d659017f07aafaa3d1b87
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-09-27 19:04:35 -0500

    Remove unnecessary comments.

--- Project 1 Regression/GridSearch.java
+++ Project 1 Regression/GridSearch.java
@@ -1,12 +1,6 @@
 public class GridSearch extends Optimizer
 {
-/*
-Its constructor should require 3 vectors as parameters, named min, max, and step. 
-Add a member variable to store each of these parameters. 
-Also, add a member variable of the vector named cur, and initialize it to be a copy of min. 
-Implements the iterate method like this:
-Make sure you understand how these two optimizers work, or ask questions about them. 
-*/
+
 	double[] cur;
 	double[] min;
 	double[] max;
